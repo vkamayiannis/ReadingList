7e42fa2b895d24a2cd6ff9edba9ff825c701cf3f
kamayiannis@yahoo.com <kamayiannis@yahoo.com>
2015-01-12 17:45:39 +0200

    categories fix

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ActiveRecord::Base
 	belongs_to :category
+	validates :category, :presence => true
+
 	scope :finished, -> {where.not(finished_on: nil)}
 	scope :recent, -> {where('finished_on > ?', 2.days.ago)}
 	#scope :search, -> (keyword){where(title: keyword) if keyword.present?}
